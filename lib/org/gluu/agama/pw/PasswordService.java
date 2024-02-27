8c94d5dd577a25c88ffae61505eabc46860e5899
gasmyr <gasmyrmougang@yahoo.fr>
2024-02-27 15:48:47 +0100

    feat: update PasswordService.java

--- lib/org/gluu/agama/pw/PasswordService.java
+++ lib/org/gluu/agama/pw/PasswordService.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 public abstract class PasswordService {
 
     public abstract boolean validate(String username, String password);
-    public abstract boolean lockAccount(String username);
+    public abstract String[] lockAccount(String username);
 
     public static PasswordService getInstance(HashMap config){
         return  new JansPasswordService(config);
