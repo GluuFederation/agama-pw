0d9dc002bf7cbe60dad80db4cfac932b8fc57e5f
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-03-01 00:22:27 +0600

    refactor: update JansPasswordService.java

--- lib/org/gluu/agama/pw/jans/JansPasswordService.java
+++ lib/org/gluu/agama/pw/jans/JansPasswordService.java
@@ -32,8 +32,8 @@ public class JansPasswordService extends PasswordService {
     public JansPasswordService(HashMap config) {
         logger.info("Flow config provided is  {}.", config);
         flowConfig = config;
-        DEFAULT_MAX_LOGIN_ATTEMPT = flowConfig.get("MAX_LOGIN_ATTEMPT") != null ? Integer.parseInt(flowConfig.get("MAX_LOGIN_ATTEMPT")) : DEFAULT_MAX_LOGIN_ATTEMPT;
-        DEFAULT_LOCK_EXP_TIME = flowConfig.get("LOCK_EXP_TIME") != null ? Integer.parseInt(flowConfig.get("LOCK_EXP_TIME")) : DEFAULT_LOCK_EXP_TIME;
+        DEFAULT_MAX_LOGIN_ATTEMPT = flowConfig.get("maxLoginAttempt") != null ? flowConfig.get("maxLoginAttempt") : DEFAULT_MAX_LOGIN_ATTEMPT;
+        DEFAULT_LOCK_EXP_TIME = flowConfig.get("lockExpTime") != null ? Integer.parseInt(flowConfig.get("lockExpTime")) : DEFAULT_LOCK_EXP_TIME;
     }
 
     public JansPasswordService() {
