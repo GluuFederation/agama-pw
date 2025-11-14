4550ffdb570e9d136ad9bcdff0c2ea5dbed41451
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-11-14 22:53:23 +0600

    refactor: update JansResetService.java

--- lib/org/gluu/agama/pw/jans/JansResetService.java
+++ lib/org/gluu/agama/pw/jans/JansResetService.java
@@ -45,13 +45,19 @@ public class JansResetService extends ResetService{
     @Override
     public Map<String, String> getUserEntityByMail(String email) {
 
+        // SessionIdService sis = CdiUtil.bean(SessionIdService.class); 
+        // sis.getSessionId(CdiUtil.bean(HttpServletRequest.class));
 
         ///
         LogUtils.log("Get User entity by mail");
-        HttpServletRequest req = CdiUtil.bean(NetworkService.class).getHttpServletRequest();
+        // HttpServletRequest req = CdiUtil.bean(NetworkService.class).getHttpServletRequest();
+
+        HttpServletRequest req = CdiUtil.bean(HttpServletRequest.class);
         LogUtils.log("REQ is  : %", req);
 
         String headerIp = req.getHeader("X-Forwarded-For");
+
+        LogUtils.log("Header ip is  : %", headerIp);
 
         /////
 
